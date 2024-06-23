2376fecd53837de6b222ae4e56f1df2f3f642d10
asmaharrabi <166958633+asmaharrabi@users.noreply.github.com>
2024-06-23 14:57:27 +0100

    final version

---
+++ app/src/main/java/com/example/myapplication/CustomAdapter.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+    private Context context;
+    Activity activity ;
+    private ArrayList<String> book_id, book_title, book_author, book_pages ;
+    Animation translate_anim ;
+    int position ;
+    CustomAdapter(Context context,
+                  Activity activity,
+                  ArrayList<String> book_id,
+                  ArrayList<String> book_title,
+                  ArrayList<String> book_author,
+                  ArrayList<String> book_pages ){
+        this.activity = activity ;
+        this.context = context ;
+        this.book_author = book_author ;
+        this.book_id = book_id ;
+        this.book_pages = book_pages ;
+        this.book_title = book_title ;
+
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent , false);
+        return new MyViewHolder(view) ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        this.position = position ;
+        holder.book_id_txt.setText(String.valueOf(book_id.get(position)));
+        holder.book_title_txt.setText(String.valueOf(book_title.get(position)));
+        holder.book_author_txt.setText(String.valueOf(book_author.get(position)));
+        holder.book_pages_txt.setText(String.valueOf(book_pages.get(position)));
+        holder.mainLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(context, UpdateActivity.class);
+                intent.putExtra("id", String.valueOf(book_id.get(position)));
+                intent.putExtra("title", String.valueOf(book_title.get(position)));
+                intent.putExtra("author", String.valueOf(book_author.get(position)));
+                intent.putExtra("pages", String.valueOf(book_pages.get(position)));
+                activity.startActivityForResult(intent, 1);
+
+            }
+        });
+    }
+    @Override
+    public int getItemCount() {
+        return book_id.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView book_id_txt , book_title_txt, book_author_txt, book_pages_txt ;
+        LinearLayout mainLayout ;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            book_id_txt = itemView.findViewById(R.id.book_id_txt);
+            book_title_txt = itemView.findViewById(R.id.book_title_txt);
+            book_author_txt = itemView.findViewById(R.id.book_author_txt);
+            book_pages_txt = itemView.findViewById(R.id.book_pages_txt);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+            translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
+            mainLayout.setAnimation(translate_anim);
+        }
+    }
+}
